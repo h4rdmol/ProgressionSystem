6931aab1b378c068cb46fcd3d50a7dcdcc9f0eff
valeriy <rotermel.valera@gmail.com>
2024-03-17 15:54:37 +0200

    removed redundant if

--- Source/ProgressionSystemRuntime/Private/Widgets/PSMenuWidget.cpp
+++ Source/ProgressionSystemRuntime/Private/Widgets/PSMenuWidget.cpp
@@ -16,44 +16,41 @@
 void UPSMenuWidget::AddImagesToHorizontalBox(int32 AmountOfUnlockedPoints, int32 AmountOfLockedPoints)
 {
 	checkf(HorizontalBox, TEXT("ERROR: 'HorizontalBox' is null"));
-	if (HorizontalBox)
+	// Loop to create and add images to the Horizontal Box for unlocked stars
+	for (int32 i = 0; i < AmountOfUnlockedPoints; i++)
 	{
-		// Loop to create and add images to the Horizontal Box for unlocked stars
-		for (int32 i = 0; i < AmountOfUnlockedPoints; i++)
+		UImage* ImageWidget = WidgetTree->ConstructWidget<UImage>(UImage::StaticClass());
+		ImageWidget->SetBrushFromTexture(UPSDataAsset::Get().GetUnlockedProgressionIcon());
+
+		// Load and set the image texture here using ImagePath or other methods
+		HorizontalBox->AddChildToHorizontalBox(ImageWidget);
+
+		// Create the UHorizontalBoxSlot and assign it to the ImageWidget
+		UHorizontalBoxSlot* HorizontalBoxSlot = Cast<UHorizontalBoxSlot>(ImageWidget->Slot);
+		if (HorizontalBoxSlot)
 		{
-			UImage* ImageWidget = WidgetTree->ConstructWidget<UImage>(UImage::StaticClass());
-			ImageWidget->SetBrushFromTexture(UPSDataAsset::Get().GetUnlockedProgressionIcon());
-
-			// Load and set the image texture here using ImagePath or other methods
-			HorizontalBox->AddChildToHorizontalBox(ImageWidget);
-
-			// Create the UHorizontalBoxSlot and assign it to the ImageWidget
-			UHorizontalBoxSlot* HorizontalBoxSlot = Cast<UHorizontalBoxSlot>(ImageWidget->Slot);
-			if (HorizontalBoxSlot)
-			{
-				HorizontalBoxSlot->SetPadding(FMargin(20.0f, 10.0f, 20.0f, 10.0f));
-				HorizontalBoxSlot->SetSize(FSlateChildSize(ESlateSizeRule::Fill));
-				HorizontalBoxSlot->SetHorizontalAlignment(HAlign_Fill);
-				HorizontalBoxSlot->SetVerticalAlignment(VAlign_Fill);
-			}
+			HorizontalBoxSlot->SetPadding(FMargin(20.0f, 10.0f, 20.0f, 10.0f));
+			HorizontalBoxSlot->SetSize(FSlateChildSize(ESlateSizeRule::Fill));
+			HorizontalBoxSlot->SetHorizontalAlignment(HAlign_Fill);
+			HorizontalBoxSlot->SetVerticalAlignment(VAlign_Fill);
 		}
+	}
 
-		// Loop to create and add images to the Horizontal Box for unlocked stars
-		for (int32 i = 0; i < AmountOfLockedPoints; i++)
+	// Loop to create and add images to the Horizontal Box for unlocked stars
+	for (int32 i = 0; i < AmountOfLockedPoints; i++)
+	{
+		UImage* ImageWidget = WidgetTree->ConstructWidget<UImage>(UImage::StaticClass());
+		ImageWidget->SetBrushFromTexture(UPSDataAsset::Get().GetLockedProgressionIcon());
+		// Load and set the image texture here using ImagePath or other methods
+		HorizontalBox->AddChildToHorizontalBox(ImageWidget);
+		// Create the UHorizontalBoxSlot and assign it to the ImageWidget
+		UHorizontalBoxSlot* HorizontalBoxSlot = Cast<UHorizontalBoxSlot>(ImageWidget->Slot);
+		if (HorizontalBoxSlot)
 		{
-			UImage* ImageWidget = WidgetTree->ConstructWidget<UImage>(UImage::StaticClass());
-			ImageWidget->SetBrushFromTexture(UPSDataAsset::Get().GetLockedProgressionIcon());
-			// Load and set the image texture here using ImagePath or other methods
-			HorizontalBox->AddChildToHorizontalBox(ImageWidget);
-			// Create the UHorizontalBoxSlot and assign it to the ImageWidget
-			UHorizontalBoxSlot* HorizontalBoxSlot = Cast<UHorizontalBoxSlot>(ImageWidget->Slot);
-			if (HorizontalBoxSlot)
-			{
-				HorizontalBoxSlot->SetPadding(FMargin(20.0f, 10.0f, 20.0f, 10.0f));
-				HorizontalBoxSlot->SetSize(FSlateChildSize(ESlateSizeRule::Fill));
-				HorizontalBoxSlot->SetHorizontalAlignment(HAlign_Fill);
-				HorizontalBoxSlot->SetVerticalAlignment(VAlign_Fill);
-			}
+			HorizontalBoxSlot->SetPadding(FMargin(20.0f, 10.0f, 20.0f, 10.0f));
+			HorizontalBoxSlot->SetSize(FSlateChildSize(ESlateSizeRule::Fill));
+			HorizontalBoxSlot->SetHorizontalAlignment(HAlign_Fill);
+			HorizontalBoxSlot->SetVerticalAlignment(VAlign_Fill);
 		}
 	}
 }
