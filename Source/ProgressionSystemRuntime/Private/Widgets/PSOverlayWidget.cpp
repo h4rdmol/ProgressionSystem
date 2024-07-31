679f613fcee18c486a1fa65105b2191cb1ec5b7f
valeriy <rotermel.valera@gmail.com>
2024-08-01 01:57:41 +0300

    Resolved warning related to the missing inline generated cpp by name

--- Source/ProgressionSystemRuntime/Private/Widgets/PSOverlayWidget.cpp
+++ Source/ProgressionSystemRuntime/Private/Widgets/PSOverlayWidget.cpp
@@ -5,6 +5,8 @@
 
 #include "Components/Image.h"
 
+#include UE_INLINE_GENERATED_CPP_BY_NAME(PSOverlayWidget)
+
 // Sets the visibility of the background overlay and lock icon.
 void UPSOverlayWidget::SetOverlayVisibility(ESlateVisibility VisibilitySlate)
 {
