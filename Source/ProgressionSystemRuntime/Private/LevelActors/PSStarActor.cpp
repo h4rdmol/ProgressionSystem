f964a910a18fd5044fa069294d0d44bddb572b59
h4rdmol <rotermel.valera@gmail.com>
2024-09-18 06:09:51 +0300

    Implemented `Hide Stars` animation on cinematics start (#12)
    * now hiding the actors if cinematic is played
    
    * fixed formatting
    
    * removed old logic with subscription to the NMM events
    
    * Added CurveTable to the data table for each character. Replaced blueprint start actor with correct class
    
    * Subscribed to a new event for cinematic, changed actor to perform animation in tick
    
    * inline init the property, removed unneeded  comma
    
    * fix
    
    * improved comments, removed redundant public
    
    * removed unused static mesh component
    
    * updated text in comments

--- Source/ProgressionSystemRuntime/Private/LevelActors/PSStarActor.cpp
+++ Source/ProgressionSystemRuntime/Private/LevelActors/PSStarActor.cpp
@@ -4,28 +4,85 @@
 #include "LevelActors/PSStarActor.h"
 
 #include "Components/StaticMeshComponent.h"
+#include "Controllers/MyPlayerController.h"
+#include "Data/PSTypes.h"
+#include "Data/PSWorldSubsystem.h"
+#include "LevelActors/PlayerCharacter.h"
+#include "MyUtilsLibraries/GameplayUtilsLibrary.h"
+#include "Subsystems/GlobalEventsSubsystem.h"
+#include "UtilityLibraries/MyBlueprintFunctionLibrary.h"
 
 #include UE_INLINE_GENERATED_CPP_BY_NAME(PSStarActor)
 
 // Sets default values
 APSStarActor::APSStarActor()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = false;
-
-}
-
-void APSStarActor::SetStaticMesh(UStaticMesh* Mesh)
-{
-	if (Mesh)
-	{
-		MeshComponentInternal->SetStaticMesh(Mesh);
-	}
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bStartWithTickEnabled = false;
 }
 
 // Called when the game starts or when spawned
 void APSStarActor::BeginPlay()
 {
 	Super::BeginPlay();
+
+	BIND_ON_LOCAL_CHARACTER_READY(this, ThisClass::OnLocalCharacterReady);
 }
 
+// Function called every frame on this Actor
+void APSStarActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	TryPlayHideStarAnimation();
+}
+
+// When a local character load finished
+void APSStarActor::OnLocalCharacterReady(APlayerCharacter* Character, int32 CharacterID)
+{
+	AMyPlayerController* LocalPC = Character ? Character->GetController<AMyPlayerController>() : nullptr;
+	if (ensureMsgf(LocalPC, TEXT("ASSERT: [%i] %hs:\n'LocalPC' is null!"), __LINE__, __FUNCTION__))
+	{
+		LocalPC->OnAnyCinematicStarted.AddDynamic(this, &APSStarActor::OnAnyCinematicStarted);
+	}
+}
+
+// Is called when any cinematic started
+void APSStarActor::OnAnyCinematicStarted(const UObject* LevelSequence, const UObject* FromInstigator)
+{
+	const UWorld* World = GetWorld();
+	check(World);
+
+	StartTimeHideStarsInternal = World->GetTimeSeconds();
+
+	// Enable tick and process this frame
+	SetActorTickEnabled(true);
+	TryPlayHideStarAnimation();
+}
+
+// Hiding stars with animation
+void APSStarActor::TryPlayHideStarAnimation()
+{
+	if (!StartTimeHideStarsInternal)
+	{
+		return;
+	}
+
+	const FPSRowData& CurrentRow = UPSWorldSubsystem::Get().GetCurrentRow();
+	if (!CurrentRow.HideStarsAnimation)
+	{
+		SetActorTickEnabled(false);
+		StartTimeHideStarsInternal = 0.f;
+		return;
+	}
+
+	const float SecondsSinceStart = GetWorld()->GetTimeSeconds() - StartTimeHideStarsInternal;
+
+	const bool bIsFinished = !UGameplayUtilsLibrary::ApplyTransformFromCurveTable(this, CurrentRow.HideStarsAnimation, SecondsSinceStart);
+	if (bIsFinished)
+	{
+		SetActorTickEnabled(false);
+		StartTimeHideStarsInternal = 0.f;
+	}
+}
--- Source/ProgressionSystemRuntime/Public/LevelActors/PSStarActor.h
+++ Source/ProgressionSystemRuntime/Public/LevelActors/PSStarActor.h
@@ -9,21 +9,31 @@ UCLASS()
 class PROGRESSIONSYSTEMRUNTIME_API APSStarActor : public AActor
 {
 	GENERATED_BODY()
-	
-public:	
+
+public:
 	// Sets default values for this actor's properties
 	APSStarActor();
 
-	UFUNCTION()
-	void SetStaticMesh(UStaticMesh* Mesh);
-	
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-	UPROPERTY()
-	TObjectPtr<class UStaticMeshComponent> MeshComponentInternal = nullptr;
 
-public:
+	/** Function called every frame on this Actor */
+	virtual void Tick(float DeltaTime) override;
 
-	
+	/** Stores the starting time to hide stars */
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadWrite, Transient, Category = "C++", meta = (BlueprintProtected, DisplayName = "Starting time to hide stars"))
+	float StartTimeHideStarsInternal = 0.0f;
+
+	/** When a local character load finished */
+	UFUNCTION(BlueprintCallable, Category= "C++", meta = (BlueprintProtected))
+	void OnLocalCharacterReady(APlayerCharacter* Character, int32 CharacterID);
+
+	/** Is called when any cinematic started */
+	UFUNCTION(BlueprintCallable, Category= "C++", meta = (BlueprintProtected))
+	void OnAnyCinematicStarted(const UObject* LevelSequence, const UObject* FromInstigator);
+
+	/** Hiding stars with animation */
+	UFUNCTION(BlueprintCallable, Category= "C++", meta = (BlueprintProtected))
+	void TryPlayHideStarAnimation();
 };
