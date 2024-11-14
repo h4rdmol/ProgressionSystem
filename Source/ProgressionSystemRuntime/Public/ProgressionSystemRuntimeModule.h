6c826879c8535009fdebaac9917f52477ea72bf3
valeriy <rotermel.valera@gmail.com>
2024-11-14 06:13:46 +0200

    added global module name, in case if we need to use it

--- Source/ProgressionSystemRuntime/Public/ProgressionSystemRuntimeModule.h
+++ Source/ProgressionSystemRuntime/Public/ProgressionSystemRuntimeModule.h
@@ -7,6 +7,8 @@
 class PROGRESSIONSYSTEMRUNTIME_API FProgressionSystemRuntimeModule : public IModuleInterface
 {
 public:
+	inline static const FName PSModuleName = TEXT("ProgressionSystem");
+
 	//~IModuleInterface
 	virtual void StartupModule() override;
 	virtual void ShutdownModule() override;
